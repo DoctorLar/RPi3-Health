6b316f871daa1097590974160f16767dad89a421
DoctorLar <doctorlar@gmail.com>
2016-04-20 07:58:01 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+# This script brings your RPi up to current versions and clean archive files out.
+sudo apt-get update --assume-yes
+sudo apt-get upgrade --assume-yes
+sudo apt-get dist-upgrade --assume-yes
+sudo apt-get autoremove –purge --assume-yes
+sudo rpi-update --assume-yes
+sudo apt-get install apt-transport-https -y --force-yes
