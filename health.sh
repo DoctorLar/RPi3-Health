1398d011997efc4b6098c81e281bc195c69bd04d
DoctorLar <doctorlar@gmail.com>
2016-04-20 07:46:05 -0500

    Update health.sh

--- health.sh
+++ health.sh
@@ -1,4 +1,4 @@
-// This script brings your pi up to current versions and clean archive files out.
+## This script brings your RPi up to current versions and clean archive files out.
 
 sudo apt-get update && sudo apt-get upgrade -y 
 sudo apt-get update && sudo apt-get dist-upgrade
