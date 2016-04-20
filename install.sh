fed23c720c7e732914e73b62528381eb19922dfa
DoctorLar <doctorlar@gmail.com>
2016-04-20 08:04:47 -0500

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get update --assume-yes
-sudo apt-get upgrade --assume-yes
-sudo apt-get dist-upgrade --assume-yes
-sudo apt-get autoremove –purge --assume-yes
-sudo rpi-update --assume-yes
-sudo apt-get install apt-transport-https -y --force-yes
